a359bc1afabca4e2d0fa26542a1592ac09f539ed
anaclara-dias <acod2105@gmail.com>
2025-03-27 11:05:08 -0300

    27/03

--- aula02/exercicios.js
+++ aula02/exercicios.js
@@ -10,3 +10,53 @@ function exercicio1()
                                                     "<br>O cubo é " + cuboN +
                                                     "</p>";
 }
+
+function exercicio2()
+{
+ let temperatura = document.getElementById("temperatura").value;
+
+ let conversao = (Number(temperatura) - Number(32)) * 5/9;
+
+ document.getElementById("mensagem").innerHTML = "<p> A temperatura em graus Fahrenheit foi" + temperatura +
+                                                    "<br>A temperatura em graus Fahrenheit é " + conversao + 
+                                                    "</p>";
+}
+
+function exercicio3()
+{
+  let valor = document.getElementById("valor").value;
+  let taxa = document.getElementById("taxa").value;
+  let tempo = document.getElementById("tempo").value;
+
+  let valorP = Number(valor) + Number(valor) * Number(taxa) * 2/100 * Number(tempo);
+  
+  document.getElementById("mensagem").innerHTML = "<p> Valores informados" +
+                                                   "<br> Valor = " + valor +
+                                                   "<br> taxa =" + taxa +
+                                                   "<br> tempo =" + tempo +
+                                                   "<br> O valor atualizado é: " + valorP +
+                                                   "</p>";
+                                                   
+}
+
+
+function exercicio4()
+{
+  let valorB = document.getElementById("valorB").value;
+  let valorA = document.getElementById("valorA").value;
+
+  let area = Number(valorB) * Number(valorA) / 2;
+
+  document.getElementById("mensagem").innerHTML = "<p> Base = " + valorB +
+                                                  "<br> Altura = " + valorA +
+                                                  "<br> A area do triangulo é: " + area +
+                                                  "</p>";
+}
+
+function exercicio5()
+{
+  let valorL = document.getElementById("valorL").value;
+   
+  let valorQ = Number(valorL)
+  
+}
