1fb76d6681a4ce6e37ef5fc44304062483edb46b
anaclara-dias <acod2105@gmail.com>
2025-04-03 11:09:01 -0300

    03/04

--- aula02/exercicios.js
+++ aula02/exercicios.js
@@ -59,7 +59,7 @@ function exercicio5()
    
   let valorQ = Number(valorL) * Number(valorL);
 
-  document.getElementById("Mensagem").innerHTML = "<p> o lado do quadrado é = " + valorL +
+  document.getElementById("mensagem").innerHTML = "<p> o lado do quadrado é = " + valorL +
                                                   "<br> A area do quadrado é: " + valorQ +
                                                   "</p>";
 }
@@ -70,23 +70,23 @@ function exercicio6()
   let num2 = document.getElementById("num2").value;
 
    if (num1 == num2){
-   "Os números são iguais!";
+    document.getElementById("mensagem").innerHTML = "Os números são iguais!";
 
    } else if (num1 != num2) {
-   "Os números são diferentes!";
+    document.getElementById("mensagem").innerHTML =  "Os números são diferentes!";
    }
 }
 
 function exercicio7()
 {
-  let num1 = document.getElementById("num1").value;
-  let num2 = document.getElementById("num2").value;
-  let num3 = document.getElementById("num3").value;
+  let num1 = Number(document.getElementById("num1").value);
+  let num2 = Number(document.getElementById("num2").value);
+  let num3 = Number(document.getElementById("num3").value);
 
-  if(num1 + num2 < num3 ){
-    "O resultado da soma é menor que o terceiro numero digitado";
+  if( (num1 + num2) < num3 ){
+    document.getElementById("mensagem").innerHTML =  "O resultado da soma é menor que o terceiro numero digitado";
   } else {
-    "O resultado da soma é maior que o terceiro numero digitado é"
+    document.getElementById("mensagem").innerHTML = "O resultado da soma é maior que o terceiro numero digitado" ;
   }
 }
 
@@ -95,20 +95,64 @@ function exercicio8()
  let salario = document.getElementById("salario").value;
 
  if (salario < 500 ){
-  mensagem = "Você tem direito ao aumento de salário";
+  document.getElementById("mensagem").innerHTML = "Você tem direito ao aumento de salário";
  } else if (salario > 500){
-  mensagem = "Você não tem direito ao aumento de salário";
+  document.getElementById("mensagem").innerHTML = "Você não tem direito ao aumento de salário";
  }
 }
 
 function exercicio9(){
-  let salario = document.getEelemntbyId("salario").value;
+  let salario = document.getElementById("salario").value;
 
   let reajuste = Number(salario) * 30 / 100;
 
-  if (salario > 500){
-    mensagem = "Você não tem direito ao aumento de salário";
-  } else if (salario <= 500){
-    mensagem = " O valor do salário reajustado fica: " + reajuste + "."; 
+  if (salario >= 500){
+    document.getElementById("mensagem").innerHTML = "Você não tem direito ao aumento de salário";
+
+  } else {
+    document.getElementById("mensagem").innerHTML = " O valor do salário reajustado fica: " + reajuste + "."; 
   }
+}
+
+function exercicio10(){
+  let cod = document.getElementById("cod").value;
+  let quantidade = document.getElementById("quantidade").value;
+
+  let preco1 = 500.00;
+  let preco = 300.00;
+
+  let valorT = Number(preco1) * Number(quantidade);
+  let valoor = Number(preco) * Number(quantidade);
+
+  if (cod == 5){
+    document.getElementById("mensagem").innerHTML = "A descrição do produto " + cod +
+                                                    ", seu preço é: " + preco1 +
+                                                    " o valor total fica: " + valorT;
+  } else
+  if (cod == 10){
+    document.getElementById("mensagem").innerHTML = "A descrição do produto " + cod +
+                                                    ", seu preço é: " + preco +
+                                                    " o valor total fica: " + valoor;
+  }
+  else{
+    document.getElementById("mensagem").innerHTML = "Código invalido";
+  }
+    
+}
+
+function exercicio11(){
+  let quantidade = document.getElementById("quantidade").value;
+  let pv = document.getElementById("pv").value;
+  let cod = document.getElementById("cod").value;
+
+ let preco = 10.00;
+ let preco1 = 50.00;
+
+  if (cod == 1){
+    document.getElementById("mensagem").innerHTML = "A quantidade é: " + quantidade +
+                                                    ", seu preço de venda é: " + pv +
+                                                    " seu valor de sub-total é de " +  +
+                                                    " o valor de frete fica: "+ +
+                                                    " Seu valor total a pagar: "+ vt;
+}
 }
